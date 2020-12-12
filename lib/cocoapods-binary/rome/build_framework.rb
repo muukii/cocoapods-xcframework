0df53fd2da685ae385b79de2eabd85f8f14b41b4
Muukii <muukii.app@gmail.com>
2020-12-12 13:48:04 +0900

    Use another approach

--- lib/cocoapods-binary/rome/build_framework.rb
+++ lib/cocoapods-binary/rome/build_framework.rb
@@ -81,7 +81,7 @@ def build_for_iosish_platform(
 
     # https://steipete.com/posts/couldnt-irgen-expression/
     other_options = [
-      "OTHER_SWIFT_FLAGS='-Xfrontend -no-serialize-debugging-options'"
+      "SWIFT_SERIALIZE_DEBUGGING_OPTIONS=NO"
     ]
 
     if bitcode_enabled
